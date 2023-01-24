5a9ec5b8d5284aade8e801d2c6a3fef950b5fdc5
Marcomussa <marcomussa567@gmail.com>
2023-01-24 10:46:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const mailer = require("nodemailer")
+const request = require('request')
+const app = express()
+const PORT = 4000
+
+async function main() {  
+    let transporter = mailer.createTransport({
+      host: "mail.bawelectric.com",
+      port: 587,
+      secure: false, 
+      auth: {
+        user: "test@bawelectric.com", 
+        pass: "Enero2023@", 
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    let mailOptions = {
+        from: '"Status Web Bawelectric" <test@bawelectric.com>',
+        to: "nicolas.harteneck@bawelectric.com, pablo.martinez@bawelectric.com", // list of receivers
+        subject: "",
+        text: "", 
+    }
+  
+    request('https://bawelectric.com/', function (error, response) {
+        if (error) {
+            mailOptions.subject = 'Error en el Sitio'
+            mailOptions.text = error
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+            })
+        } else if(response.statusCode === 200) {
+            mailOptions.subject = 'Sitio en Linea'
+            mailOptions.text = "Estado HTTPS: 200 , Sitio Funcionado Correctamente"
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+            })
+        } else {
+            mailOptions.subject = 'Error en el Sitio'
+            mailOptions.text = error    
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+            })
+        }
+    });
+}
+
+main().catch(console.error);
+
+app.listen(PORT, () => {
+    setInterval(sendEmail, 1000 * 60 * 60 * 6) // Cada 6 Horas Envia Notificacion. 
+    console.log(`Server on PORT: ${PORT}`)
+})
